4a338bb7f1e6f43158d38b959631103cf9726608
albertoalves34 <155261962+albertoalves34@users.noreply.github.com>
2026-04-18 20:14:52 -0300

    reverter para a pnultima versão

--- Banco.hs
+++ Banco.hs
@@ -1,52 +1,31 @@
-
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
 
-module Banco where
-
+import Database.SQLite.Simple
 import Data.Text (Text)
-import Data.Aeson (ToJSON, FromJSON)
-import GHC.Generics (Generic)
 
--- Tipo User (simulado em memória)
-data User = User Int String  -- id, email
-  deriving (Show, Generic)
-
-instance ToJSON User
-instance FromJSON User
-
-type Connection = ()
-
--- Adicionar usuário (simulado)
 addUser :: Connection -> String -> String -> IO ()
-addUser _ email password = do
-  putStrLn $ "Usuário " ++ email ++ " criado com sucesso!"
+addUser conn email password = do
+  execute conn 
+    "INSERT INTO users (email, password) VALUES (?, ?)" 
+    (email, password)
 
--- Buscar usuário por email (simulado)
-getUserByEmail :: Connection -> String -> IO (Maybe (Int, String))
-getUserByEmail _ email = do
-  if email == "test@email.com"
-    then return $ Just (1, "password123")
-    else return Nothing
-
--- Buscar usuário por ID (simulado)
-getUserById :: Connection -> Int -> IO (Maybe User)
-getUserById _ userId = do
-  if userId == 1
-    then return $ Just (User 1 "test@email.com")
-    else return Nothing
-
--- Listar todos os usuários (simulado)
-listUsers :: Connection -> IO [User]
-listUsers _ = do
-  return [User 1 "test@email.com", User 2 "user@email.com"]
-
--- Deletar usuário (simulado)
 deleteUser :: Connection -> Int -> IO ()
-deleteUser _ userId = do
-  putStrLn "Usuário deletado!"
+deleteUser conn UserId = do
+    execute conn "DELETE FROM users WHERE ID = ?" (Only user id)
 
--- Atualizar senha (simulado)
-updateUserPassword :: Connection -> Int -> String -> IO ()
-updateUserPassword _ userId newPassword = do
-  putStrLn "Senha atualizada!"
+getUserByEmail :: Connection -> String -> IO (Maybe String)
+getUser conn email = do
+  return Nothing
+
+getUserById :: Connection -> Int -> IO (Maybe (Int, String))
+getUserById conn userId = do
+  results <- query conn 
+    "SELECT id, email FROM users WHERE id = ?" 
+    (Only userId)
+  return $ case results of
+    [user] -> Just user
+    _ -> Nothing
+    
+listUsers :: Connection -> IO [User]
+listUsers conn = do
+  query_ conn "SELECT id, email FROM users"
