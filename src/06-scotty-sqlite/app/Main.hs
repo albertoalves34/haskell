3a5abcd98d89b8b882003007ea6d2ea226cf1c22
albertoalves34 <155261962+albertoalves34@users.noreply.github.com>
2026-05-01 23:54:13 -0300

    limpado arquivos desnecessarios

--- src/06-scotty-sqlite/app/Main.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-import Web.Scotty (get, scotty, text)
-
-main :: IO ()
-main =
-  scotty 3000 $ do
-    get "/healthz" $ text "ok"
-    get "/" $ text "demo-scotty-sqlite running"
